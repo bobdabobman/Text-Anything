e418238d404f1ce558a2e362dc21c5f7dcdfd009
zwingthomas <zwingthomas@gmail.com>
2024-09-29 01:56:35 +0300

    strange error, unable to make bucket outside eu

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state"
     key            = "terraform/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-central-1"           # eu regulations?
     dynamodb_table = "terraform-lock-table"
     encrypt        = true                     # Enable encryption at rest
   }
