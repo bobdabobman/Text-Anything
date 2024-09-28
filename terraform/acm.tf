18de28f6baf0e8a32b886bd607fbdeb22c977f60
zwingthomas <zwingthomas@gmail.com>
2024-09-29 01:13:46 +0300

    certificate creation

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,4 +1,16 @@
+# Create certificate
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  tags = {
+    Name = "my-acm-cert"
+  }
+}
+
+# Allow it to be referencable
 data "aws_acm_certificate" "cert" {
   domain   = var.domain_name
   statuses = ["ISSUED"]
+  most_recent = true
 }
