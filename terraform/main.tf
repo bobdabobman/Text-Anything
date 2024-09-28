263accf10a328073639c6755ea121d4f25cdabe1
zwingthomas <zwingthomas@gmail.com>
2024-09-29 02:47:23 +0300

    tfstate remote

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state"
+    bucket         = "terraform-state-354923279633"
     key            = "app/terraform.tfstate"      # Unique key for the app
-    region         = "eu-central-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
