59766e39cdc90c11306bd607c26907237f4110da
zwingthomas <zwingthomas@gmail.com>
2024-09-28 21:11:47 +0300

    Terraform name conflicts

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -29,14 +29,3 @@ resource "aws_lb_target_group" "app_tg" {
     Name = "${var.project_name}-tg"
   }
 }
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
