9c59523255e1357c9cfb58fd2b4603112d62137d
zwingthomas <zwingthomas@gmail.com>
2024-09-29 02:01:25 +0300

    terraform state bucket errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state"
-    key            = "terraform/terraform.tfstate"
-    region         = "eu-central-1"           # eu regulations?
+    key            = "app/terraform.tfstate"      # Unique key for the app
+    region         = "eu-central-1"
     dynamodb_table = "terraform-lock-table"
-    encrypt        = true                     # Enable encryption at rest
+    encrypt        = true
   }
 }
+
+
