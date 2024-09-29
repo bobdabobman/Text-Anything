db83e0c3b3ae838cef2e79360024fcb2b99be570
zwingthomas <zwingthomas@gmail.com>
2024-09-29 03:33:58 +0300

    specify two subnets for alb

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -20,15 +20,18 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[0]
+}
 
-  tags = {
-    Name = "${var.project_name}-subnet-public"
-  }
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"  # Should be in a different region for HA
+  map_public_ip_on_launch = true
 }
 
 resource "aws_route_table" "public" {
