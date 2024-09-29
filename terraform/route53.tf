e82b1940d74e1d85b5adb62010b4d5cbe7c54a57
zwingthomas <zwingthomas@gmail.com>
2024-09-29 11:16:42 +0300

    Pause on the Route 53 nonsense

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -45,39 +45,39 @@
 # }
 
 
-resource "aws_route53_zone" "main" {
-  name = var.domain_name
-}
+# resource "aws_route53_zone" "main" {
+#   name = var.domain_name
+# }
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      value  = dvo.resource_record_value
-    }
-  }
+# resource "aws_route53_record" "cert_validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       type   = dvo.resource_record_type
+#       value  = dvo.resource_record_value
+#     }
+#   }
 
-  zone_id = aws_route53_zone.main.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.value]
-  ttl     = 300
-}
+#   zone_id = aws_route53_zone.main.zone_id
+#   name    = each.value.name
+#   type    = each.value.type
+#   records = [each.value.value]
+#   ttl     = 300
+# }
 
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "cert" {
+#   certificate_arn         = aws_acm_certificate.cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+# }
 
-resource "aws_route53_record" "app_alias" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = var.domain_name
-  type    = "A"
+# resource "aws_route53_record" "app_alias" {
+#   zone_id = aws_route53_zone.main.zone_id
+#   name    = var.domain_name
+#   type    = "A"
 
-  alias {
-    name                   = aws_lb.alb.dns_name
-    zone_id                = aws_lb.alb.zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name                   = aws_lb.alb.dns_name
+#     zone_id                = aws_lb.alb.zone_id
+#     evaluate_target_health = false
+#   }
+# }
