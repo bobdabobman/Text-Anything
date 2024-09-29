574a64467d08d0c4aa4fb5a540e4d114aa30edea
zwingthomas <zwingthomas@gmail.com>
2024-09-29 11:53:01 +0300

    typo

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "project_name" {
   description = "Name prefix for all resources"
-  default     = "hello-world-app"
+  default     = "hello-world"
 }
 
 variable "twilio_auth_token" {
